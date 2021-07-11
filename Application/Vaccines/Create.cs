10b19a38a76050b24e57845b6f66b0439dd73835
bleart nuredini <nuredini.bleart@gmail.com>
2021-07-11 14:31:56 +0200

    fix vaccine creation

--- Application/Vaccines/Create.cs
+++ Application/Vaccines/Create.cs
@@ -10,53 +10,53 @@ using Persistence;
 
 namespace Application.Vaccines
 {
-  public class Create
-  {
-    public class Command : IRequest<Result<Unit>>
+    public class Create
     {
-      public Vaccine Vaccine { get; set; }
-    }
-
-    public class CommandValidator : AbstractValidator<Command>
-    {
-      public CommandValidator()
-      {
-        RuleFor(x => x.Vaccine).SetValidator(new VaccineValidator());
-      }
-    }
-
-    public class Handler : IRequestHandler<Command, Result<Unit>>
-    {
-      private readonly DataContext _context;
-      private readonly IUserAccessor _userAccessor;
-
-      public Handler(DataContext context, IUserAccessor userAccessor)
-      {
-        _context = context;
-        _userAccessor = userAccessor;
-      }
-
-      public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
-      {
-        var user = await _context.Users.FirstOrDefaultAsync(x =>
-            x.UserName == _userAccessor.GetUsername());
-
-        var patient = new PatientVaccine
+        public class Command : IRequest<Result<Unit>>
         {
-          AppUser = user,
-          Vaccine = request.Vaccine
-        };
-
-        request.Vaccine.Patients.Add(patient);
-        _context.Vaccines.Add(request.Vaccine);
-
-        if (!(await _context.SaveChangesAsync() > 0))
-        {
-          return Result<Unit>.Failure("Failed during Vaccine creation");
+            public Vaccine Vaccine { get; set; }
         }
 
-        return Result<Unit>.Success(Unit.Value);
-      }
+        public class CommandValidator : AbstractValidator<Command>
+        {
+            public CommandValidator()
+            {
+                RuleFor(x => x.Vaccine).SetValidator(new VaccineValidator());
+            }
+        }
+
+        public class Handler : IRequestHandler<Command, Result<Unit>>
+        {
+            private readonly DataContext _context;
+            private readonly IUserAccessor _userAccessor;
+
+            public Handler(DataContext context, IUserAccessor userAccessor)
+            {
+                _context = context;
+                _userAccessor = userAccessor;
+            }
+
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
+            {
+                // var user = await _context.Users.FirstOrDefaultAsync(x =>
+                //     x.UserName == _userAccessor.GetUsername());
+
+                // var patient = new PatientVaccine
+                // {
+                //   AppUser = user,
+                //   Vaccine = request.Vaccine
+                // };
+
+                // request.Vaccine.Patients.Add(patient);
+                _context.Vaccines.Add(request.Vaccine);
+
+                if (!(await _context.SaveChangesAsync() > 0))
+                {
+                    return Result<Unit>.Failure("Failed during Vaccine creation");
+                }
+
+                return Result<Unit>.Success(Unit.Value);
+            }
+        }
     }
-  }
 }
