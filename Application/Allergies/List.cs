76961b5090b2fef068dd7faa00eb984213296833
Qendresa Berisha <73610620+QendresaBerisha@users.noreply.github.com>
2021-07-12 02:32:51 +0200

    Patient allergy relation

--- Application/Allergies/List.cs
+++ Application/Allergies/List.cs
@@ -2,6 +2,8 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Core;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -11,19 +13,25 @@ namespace Application.Allergies
 {
     public class List
     {
-        public class Query : IRequest<Result<List<Allergy>>> { }
+        public class Query : IRequest<Result<List<AllergyDto>>> { }
 
-        public class Handler : IRequestHandler<Query, Result<List<Allergy>>>
+        public class Handler : IRequestHandler<Query, Result<List<AllergyDto>>>
         {
             private readonly DataContext _context;
-            public Handler(DataContext context)
+            private readonly IMapper _mapper;
+            public Handler(DataContext context, IMapper mapper)
             {
                 _context = context;
+                _mapper = mapper;
             }
 
-            public async Task<Result<List<Allergy>>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<Result<List<AllergyDto>>> Handle(Query request, CancellationToken cancellationToken)
             {
-                return Result<List<Allergy>>.Success(await _context.Allergies.ToListAsync());
+                var allergy = await _context.Allergies
+                                    .ProjectTo<AllergyDto>(_mapper.ConfigurationProvider)
+                                    .ToListAsync(cancellationToken);
+
+                return Result<List<AllergyDto>>.Success(allergy);
             }
         }
     }
