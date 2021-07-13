55c9c86393dd457b6cd9474b72389d8c63b1bdf3
bleart nuredini <nuredini.bleart@gmail.com>
2021-07-14 01:13:52 +0200

    update user profile components

--- client/src/app/models/profile.ts
+++ client/src/app/models/profile.ts
@@ -3,23 +3,27 @@ import { User } from "./user";
 export interface Profile {
   username: string;
   displayName: string;
-  bio:string;
+  bio: string;
 }
+
 export class Profile implements Profile {
   constructor(user: User) {
-      this.username = user.username;
-      this.displayName = user.displayName;
+    this.username = user.username;
+    this.displayName = user.displayName;
   }
 }
+
 export interface UserChronicDisease {
   id: string;
   name: string;
 }
+
 export interface UserAllergy {
-    id: string;
-    type: string;
-    causes: string;
+  id: string;
+  type: string;
+  causes: string;
 }
+
 export interface UserVaccine {
   id: string;
   name: string;
@@ -27,4 +31,3 @@ export interface UserVaccine {
   creator: string;
   type: string;
 }
-
