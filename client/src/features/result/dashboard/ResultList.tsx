429281dda6499b935bc92284fbd49f5b6aef0eed
Dante-Berishaj <73610454+Dante-Berishaj@users.noreply.github.com>
2021-07-15 01:00:06 +0200

    result crud

---
+++ client/src/features/result/dashboard/ResultList.tsx
@@ -0,0 +1,57 @@
+
+import { observer } from "mobx-react-lite";
+import React, { SyntheticEvent, useState } from "react";
+import { Button, Item, Segment } from "semantic-ui-react";
+import { useStore } from "../../../app/stores/store";
+
+export default observer(function ResultList() {
+  const { resultStore } = useStore();
+  const { deleteResult, results, loading } = resultStore;
+  const [target, setTarget] = useState("");
+
+  function handleResultDelete(
+    e: SyntheticEvent<HTMLButtonElement>,
+    id: string
+  ) {
+    setTarget(e.currentTarget.name);
+    deleteResult(id);
+  }
+
+  return (
+    <Segment>
+      <Item.Group divided>
+        {results.map((result) => {
+          return (
+            <Item key={result.id}>
+              <Item.Content>
+                <Item.Header as="a">{result.result}</Item.Header>
+                <Item.Description>
+                  <div>{result.testName}</div>
+                  <div>
+                    {result.date}, {result.patients}
+                  </div>
+                </Item.Description>
+                <Item.Extra>
+                  <Button
+                    onClick={() => resultStore.selectResult(result.id)}
+                    floated="right"
+                    content="Shiko"
+                    color="blue"
+                  />
+                  <Button
+                    name={result.id}
+                    loading={loading && target === result.id}
+                    onClick={(e) => handleResultDelete(e, result.id)}
+                    floated="right"
+                    content="Fshij"
+                    color="red"
+                  />
+                </Item.Extra>
+              </Item.Content>
+            </Item>
+          );
+        })}
+      </Item.Group>
+    </Segment>
+  );
+});
---
+++ client/src/features/result/form/ResultForm.tsx
@@ -0,0 +1,73 @@
+import { Formik, Form } from "formik";
+import { observer } from "mobx-react-lite";
+import React, { useState } from "react";
+import { Button, Segment } from "semantic-ui-react";
+import { useStore } from "../../../app/stores/store";
+import * as Yup from "yup";
+import MyTextInput from "../../../app/common/form/MyTextInput";
+import { Result } from "../../../app/models/results";
+import MyDateInput from "../../../app/common/form/MyDateInput";
+
+export default observer(function ResultForm() {
+  const { resultStore } = useStore();
+  const { selectedResult, closeForm, createResult, updateResult, loading } =
+    resultStore;
+
+  const initialState = selectedResult ?? {
+    id: "",
+    result: "",
+    testName: "",
+    date: null,
+  };
+
+  const validationSchema = Yup.object({
+    result: Yup.string().required(),
+    testName: Yup.string().required(),
+    date: Yup.string().required("Ju lutem vendosni nje date").nullable(),
+  });
+  const [result] = useState(initialState);
+
+  function handleFormSubmit(result: Result) {
+    result.id ? updateResult(result) : createResult(result);
+  }
+
+  return (
+    <Segment clearing>
+      <Formik
+        validationSchema={validationSchema}
+        enableReinitialize
+        initialValues={result}
+        onSubmit={(values) => handleFormSubmit(values)}
+      >
+        {({ handleSubmit, isValid, isSubmitting, dirty }) => (
+          <Form className="ui form" onSubmit={handleSubmit}>
+            <MyTextInput name="result" placeholder="Rezultati" />
+            <MyTextInput name="testName" placeholder="Emri Testit" />
+            <MyTextInput name="creator" placeholder="Krijuesi i vaksines" />
+            <MyDateInput
+              placeholderText="Data"
+              name="date"
+              showTimeSelect
+              timeCaption="time"
+              dateFormat="dd.MM.yyyy, (h:mm aa)"
+            />
+            <Button
+              disabled={isSubmitting || !dirty || !isValid}
+              loading={loading}
+              floated="right"
+              positive
+              type="submit"
+              content="Ruaj"
+            />
+            <Button
+              onClick={closeForm}
+              floated="right"
+              type="button"
+              content="Anulo"
+            />
+          </Form>
+        )}
+      </Formik>
+    </Segment>
+  );
+});
